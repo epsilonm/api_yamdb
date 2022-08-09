d2c453bc026fa67286882a07e5e6dddfa5049744
Марк Губин <epsilonmga@yandex.ru>
2022-08-09 21:28:53 +0500

    Add: add custom user model to users/models

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    bio = models.TextField(
+        'Biography',
+        blank=True,
+    )
