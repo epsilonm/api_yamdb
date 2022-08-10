c7c08163312ab99f1c32c31a3ab2f65c1b63515e
Марк Губин <epsilonmga@yandex.ru>
2022-08-10 13:10:19 +0500

    Feature: add fields to user model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,7 +3,37 @@ from django.db import models
 
 
 class User(AbstractUser):
+    USER_ROLES = (
+        ('user', 'User'),
+        ('moderator', 'Moderator'),
+        ('admin', 'Admin'),
+    )
     bio = models.TextField(
         'Biography',
         blank=True,
     )
+    username = models.CharField(
+        verbose_name='Username',
+        unique=True,
+        max_length=150,
+    )
+    email = models.EmailField(
+        verbose_name='Email',
+        unique=True,
+        max_length=254
+    )
+    first_name = models.CharField(
+        verbose_name='First name',
+        max_length=150,
+    )
+    last_name = models.CharField(
+        verbose_name='Last name',
+        max_length=150,
+    )
+    role = models.CharField(max_length=9,
+                            choices=USER_ROLES,
+                            default='user')
+    is_active = True
+    confirmation_code = models.CharField(max_length=100, blank=True)
+
+    REQUIRED_FIELDS = ['email']
