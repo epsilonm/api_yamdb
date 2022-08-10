bbdd54b077c45805c866e2515dc8a16247a7a917
Артём Ширнин <artyom.shirnin@yandex.ru>
2022-08-10 18:52:19 +1000

    Создание моделей Category, Genre, Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,20 +3,39 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(
-        verbose_name='Название',
-        max_length=200
+        max_length=256,
+        verbose_name='Название'
     )
 
 
 class Genre(models.Model):
     name = models.CharField(
-        verbose_name='Название',
-        max_length=200
+        max_length=256,   
+        verbose_name='Название'
     )
 
 
 class Title(models.Model):
     name = models.CharField(
-        verbose_name='Название',
-        max_length=200
+        max_length=256,
+        verbose_name='Название'
+    )
+    year = models.IntegerField(
+        verbose_name='Год выпуска'
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        null=True,
+        blank=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        verbose_name='Жанр'
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        null=True
     )
