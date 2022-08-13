b7463750fcd1fa61ab81a3f25e4a81de18667c47
Михаил Шутов <mihvs22@gmail.com>
2022-08-13 21:45:14 +0300

    added feature csv to bd

---
+++ api_yamdb/reviews/management/commands/csv_to_bd.py
@@ -0,0 +1,40 @@
+import csv
+import os
+
+from django.core.management.base import BaseCommand
+from reviews.models import Comment, Review, Category, Genre, Title, GenreTitle
+from users.models import User
+
+data = {
+    User: 'users.csv',
+    Category: 'category.csv',
+    Genre: 'genre.csv',
+    Title: 'titles.csv',
+    GenreTitle: 'genre_title.csv',
+    Review: 'review.csv',
+    Comment: 'comments.csv'
+}
+
+
+def read_csv(name_file):
+    path = os.path.join('static/data', name_file)
+    data_list = []
+    with open(path, encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=',')
+        for row in reader:
+            data_list.append(row)
+    return data_list
+
+
+def write_in_bd(data_dict):
+    for key, value in data_dict.items():
+        data_list = read_csv(name_file=value)
+        for i in data_list:
+            key.objects.create(**i)
+
+
+class Command(BaseCommand):
+    help = 'Импортирует данные из csv в базу данных'
+
+    def handle(self, *args, **options):
+        write_in_bd(data_dict=data)
