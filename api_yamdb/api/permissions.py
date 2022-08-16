d6a25bac59ef5b8c56896b2bd7f654ea99d400ad
Марк Губин <epsilonmga@yandex.ru>
2022-08-17 00:11:11 +0500

    Change permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,17 +4,16 @@ from rest_framework import permissions
 class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            if request.user.role == 'admin' or request.user.is_superuser:
-                return True
+        if request.user.is_authenticated and (request.user.role == 'admin'
+                                              or request.user.is_superuser):
+            return True
         return False
 
 
 class IsModeratorUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            if request.user.role == 'moderator':
-                return True
+        if request.user.is_authenticated and request.user.role == 'moderator':
+            return True
         return False
 
 
@@ -40,5 +39,4 @@ class IsAdminOrReadOnly(permissions.BasePermission):
                 or (request.user.is_authenticated
                     and request.user.role == 'admin'))
 
-    def has_object_permission(self, request, view, obj):
-        return request.user.role == 'admin'
+
