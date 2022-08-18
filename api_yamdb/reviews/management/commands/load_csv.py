bb20a28ec06ff0ea671f440fbc4e1e22906e956a
Михаил Шутов <mihvs22@gmail.com>
2022-08-19 01:12:54 +0300

    script: Refactoring load_csv.py

--- api_yamdb/reviews/management/commands/load_csv.py
+++ api_yamdb/reviews/management/commands/load_csv.py
@@ -8,75 +8,67 @@ from reviews.models import Comment, Review, Category, Genre, Title
 from users.models import User
 
 
+DATA = {
+    User: 'users.csv',
+    Category: 'category.csv',
+    Genre: 'genre.csv',
+    Title: 'titles.csv',
+    Review: 'review.csv',
+    Comment: 'comments.csv'
+}
+
+
 def read_csv(name_file):
+    """Считывает данные из csv и возвращает список строк таблицы"""
     path = os.path.join('static/data', name_file)
-    data_list = []
     with open(path, encoding='utf-8') as csv_file:
         reader = csv.DictReader(csv_file, delimiter=',')
-        for row in reader:
-            data_list.append(row)
-    return data_list
+        return list(reader)
 
 
-def load_data(filds):
-    pass
+def get_list_fields_model(model):
+    """
+    Принимает объект модели, и возвращает словарь с полями в виде:
+    {<поле модели>: <поле в таблице>}
+    """
+    fields_obj_list = model._meta.fields
+    fields = {
+        field.name: field.attname for field in fields_obj_list
+    }
+    return fields
 
 
-def load_users():
-    data_list = read_csv('users.csv')
-    users = [
-        User(
-            id=row['id'],
-            username=row['username'],
-            email=row['email'],
-            role=row['role']
-        )
-        for row in data_list
-    ]
-    User.objects.bulk_create(users)
+def changes_fields(fields_model, table):
+    """
+    Изменяет название полей прочитанной таблицы
+    для корректной записи в БД
+    """
+    for row in table:
+        for name_field in list(row):
+            if (
+                    name_field in fields_model
+                    and name_field
+                    != fields_model[name_field.replace("_id", "")]
+            ):
+                row[fields_model[name_field]] = row.pop(name_field)
 
 
-def load_categories():
-    data_list = read_csv('category.csv')
-    categories = [
-        Category(
-            id=row['id'],
-            name=row['name'],
-            slug=row['slug']
-        )
-        for row in data_list
-    ]
-    Category.objects.bulk_create(categories)
-
-
-def load_genres():
-    data_list = read_csv('genre.csv')
-    genres = [
-        Genre(
-            id=row['id'],
-            name=row['name'],
-            slug=row['slug']
-        )
-        for row in data_list
-    ]
-    Genre.objects.bulk_create(genres)
-
-
-def load_titles():
-    data_list = read_csv('titles.csv')
-    titles = [
-        Title(
-            id=row['id'],
-            name=row['name'],
-            year=row['year'],
-            category=Category.objects.get(id=row['category'])
-        )
-        for row in data_list
-    ]
-    Title.objects.bulk_create(titles)
+def load_data(model, name_file):
+    """
+    Загрузка данных по имени модели.
+    Не загружает данные во вспомогательную таблицу
+    со связью многие ко многим
+    """
+    table = read_csv(name_file)
+    changes_fields(get_list_fields_model(model), table)
+    model.objects.bulk_create(model(**row) for row in table)
 
 
 def load_genre_title():
+    """
+    Загружает данные во вспомогательную таблицу
+    для моделей со связью многие ко многим
+    """
     data_list = read_csv('genre_title.csv')
     [
         Title.objects.get(id=row['title_id']).genre.add(row['genre_id'])
@@ -84,35 +76,9 @@ def load_genre_title():
     ]
 
 
-def load_review():
-    data_list = read_csv('review.csv')
-    reviews = [
-        Review(
-            id=row['id'],
-            text=row['text'],
-            author=User.objects.get(id=row['author']),
-            title=Title.objects.get(id=row['title_id']),
-            score=row['score'],
-            pub_date=row['pub_date']
-        )
-        for row in data_list
-    ]
-    Review.objects.bulk_create(reviews)
-
-
-def load_comments():
-    data_list = read_csv('comments.csv')
-    comments = [
-        Comment(
-            id=row['id'],
-            text=row['text'],
-            author=User.objects.get(id=row['author']),
-            review=Review.objects.get(id=row['review_id']),
-            pub_date=row['pub_date']
-        )
-        for row in data_list
-    ]
-    Comment.objects.bulk_create(comments)
+def del_data():
+    for model in DATA:
+        model.objects.all().delete()
 
 
 class Command(BaseCommand):
@@ -120,146 +86,41 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '-u',
-            '--users',
-            action='store_true',
-            help='Импортирует пользователей из csv в базу данных'
-        )
-        parser.add_argument(
-            '-cat',
-            '--categories',
-            action='store_true',
-            help='Импортирует категории из csv в базу данных'
-        )
-        parser.add_argument(
-            '-g',
-            '--genres',
-            action='store_true',
-            help='Импортирует жанры из csv в базу данных'
-        )
-        parser.add_argument(
-            '-t',
-            '--titles',
-            action='store_true',
-            help='Импортирует произведения из csv в базу данных'
-        )
-        parser.add_argument(
-            '-gt',
-            '--genre_title',
-            action='store_true',
-            help='Импортирует отношения жанров с произведениями'
-                 ' из csv в базу данных'
-        )
-        parser.add_argument(
-            '-rv',
-            '--reviews',
-            action='store_true',
-            help='Импортирует отзывы из csv в базу данных'
-        )
-        parser.add_argument(
-            '-com',
-            '--comments',
-            action='store_true',
-            help='Импортирует комментарии из csv в базу данных'
-        )
-        parser.add_argument(
             '-a',
             '--all',
             action='store_true',
             help='Импортирует все таблицы из csv в базу данных'
         )
-
-        def get_option(self):
-            options = {
-
-            }
+        parser.add_argument(
+            '-d',
+            '--delete',
+            action='store_true',
+            help='Удаляет все данные из базы данных'
+        )
 
     def handle(self, *args, **options):
-        if options['users']:
-            try:
-                load_users()
-                self.stdout.write(
-                    self.style.SUCCESS('Пользователи загружены в БД'))
-            except django.db.utils.IntegrityError:
-                self.stdout.write(
-                    self.style.WARNING('Пользователи уже существуют'))
-        elif options['categories']:
-            try:
-                load_categories()
-                self.stdout.write(
-                    self.style.SUCCESS('Категории загружены в БД'))
-            except django.db.utils.IntegrityError:
-                self.stdout.write(
-                    self.style.WARNING('Категории уже существуют'))
-        elif options['genres']:
-            try:
-                load_genres()
-                self.stdout.write(
-                    self.style.SUCCESS('Жанры загружены в БД'))
-            except django.db.utils.IntegrityError:
-                self.stdout.write(
-                    self.style.WARNING('Жанры уже существуют'))
-        elif options['titles']:
-            try:
-                load_titles()
-                self.stdout.write(
-                    self.style.SUCCESS('Произведения загружены в БД'))
-            except django.db.utils.IntegrityError:
-                self.stdout.write(
-                    self.style.WARNING('Произведения уже существуют'))
-            except ObjectDoesNotExist:
-                self.stdout.write(
-                    self.style.NOTICE('Нет данных из связанных таблиц'))
-        elif options['genre_title']:
-            try:
+        try:
+            if options['all']:
+                for model, name_file in DATA.items():
+                    load_data(model, name_file)
                 load_genre_title()
                 self.stdout.write(
-                    self.style.SUCCESS('Отношения жанров с произведениями'
-                                       ' загружены в БД'))
-            except django.db.utils.IntegrityError:
+                    self.style.SUCCESS('Таблицы загружены в базу данных.'))
+            elif options['delete']:
+                del_data()
                 self.stdout.write(
-                    self.style.WARNING('Отношения жанров с произведениями '
-                                       'уже существуют'))
-            except ObjectDoesNotExist:
+                    self.style.SUCCESS('База данных успешно очищена.'))
+            else:
                 self.stdout.write(
-                    self.style.NOTICE('Нет данных из связанных таблиц'))
-        elif options['reviews']:
-            try:
-                load_review()
-                self.stdout.write(
-                    self.style.SUCCESS('Отзывы загружены в БД'))
-            except django.db.utils.IntegrityError:
-                self.stdout.write(
-                    self.style.WARNING('Отзывы уже существуют'))
-            except ObjectDoesNotExist:
-                self.stdout.write(
-                    self.style.NOTICE('Нет данных из связанных таблиц'))
-        elif options['comments']:
-            try:
-                load_comments()
-                self.stdout.write(
-                    self.style.SUCCESS('Комментарии загружены в БД'))
-            except django.db.utils.IntegrityError:
-                self.stdout.write(
-                    self.style.WARNING('Комментарии уже существуют'))
-            except ObjectDoesNotExist:
-                self.stdout.write(
-                    self.style.NOTICE('Нет данных из связанных таблиц'))
-        elif options['all']:
-            try:
-                load_users()
-                load_categories()
-                load_genres()
-                load_titles()
-                load_genre_title()
-                load_review()
-                load_comments()
-                self.stdout.write(
-                    self.style.SUCCESS('Таблицы загружены в БД'))
-            except Exception as e:
-                self.stdout.write(self.style.ERROR('Ошибка загрузки данных:'
-                                                   ' "%s"' % e))
-        else:
+                    self.style.SQL_KEYWORD('Команда используется с ключом,'
+                                           ' список всех ключей: --help'))
+        except django.db.utils.IntegrityError as e:
             self.stdout.write(
-                self.style.SQL_KEYWORD('Команда используется с ключом,'
-                                       ' список всех ключей: --help'))
+                self.style.ERROR('Ошибка загрузки. База данных не пуста. '
+                                 'Совпадение уникальных полей. "%s"' % e))
+        except ObjectDoesNotExist:
+            self.stdout.write(
+                self.style.NOTICE('Нет данных из связанных таблиц'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR('Ошибка загрузки данных:'
+                                               ' "%s"' % e))
