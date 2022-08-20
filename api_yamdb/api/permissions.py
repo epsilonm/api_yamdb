c21e6b4af6a778acba1c325adc00339f7e0703dd
Марк Губин <epsilonmga@yandex.ru>
2022-08-21 01:20:13 +0500

    Refactor: refactor api/permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,8 +4,7 @@ from rest_framework import permissions
 class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return (request.user.is_authenticated
-                and (request.user.is_admin))
+        return request.user.is_authenticated and request.user.is_admin
 
 
 class IsOwnerAdminModeratorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
