22813449938f71d5de53e386d4584b711211ab05
Марк Губин <epsilonmga@yandex.ru>
2022-08-20 23:55:45 +0500

    Refactor: refactor users/validators

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,7 +5,7 @@ class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (request.user.is_authenticated
-                and (request.user.is_admin or request.user.is_superuser))
+                and (request.user.is_admin))
 
 
 class IsOwnerAdminModeratorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
@@ -13,9 +13,7 @@ class IsOwnerAdminModeratorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.is_superuser
                 or request.user.is_admin
-                or request.user.is_staff
                 or request.user.is_moderator
                 or request.user == obj.author)
 
@@ -26,4 +24,4 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated
-                    and request.user.role == 'admin'))
+                    and request.user.is_admin))
--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -1,13 +1,9 @@
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
-import re
+
 
 def validate_username(value):
     if value.lower() == 'me':
         raise ValidationError(
             _(f'{value} служебное имя!')
-        )
-    elif re.findall('[@_.+-]', value):
-        raise ValidationError(
-            _(f'{value} не должно содержать символы "@_.+="!')
         )
